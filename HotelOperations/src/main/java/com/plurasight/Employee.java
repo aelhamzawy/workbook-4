a57c7fabd1cd736540d951c1dac1471737f67e48
Ahmed Elhamzawy <aelhamzawy2020@gmail.com>
2025-05-05 14:47:40 -0800

    modifying the Employee by adding some extra methods

--- HotelOperations/src/main/java/com/plurasight/Employee.java
+++ HotelOperations/src/main/java/com/plurasight/Employee.java
@@ -1,39 +1,117 @@
 package com.plurasight;
 
-// This is THe Employee Class To Get All The Information About THe Employee
+/// This is THe Employee Class To Get All The Information About THe Employee
 public class Employee {
 
-    // Creating The Main Reservation Variables
+    /// Creating The Main Reservation Variables
+    private String name;
+    private int employeeId;
+    private String department;
+    private float payeRate;
+    private float hoursWorked;
     private float totalPay;
     private float regularHours;
     private float overTimeHours;
+    private double punchIn;
+    private double punchOut;
 
-    // Creating The Constructor For The Reservation Class
-
-    public Employee(float overTimeHours, float regularHours, float totalPay) {
-        this.overTimeHours = overTimeHours;
-        this.regularHours = regularHours;
+    /// Creating The Constructor For The Reservation Class
+    public Employee(String name, int employeeId, String department, float payeRate, float hoursWorked, float totalPay, float regularHours, float overTimeHours, double punchIn, double punchOut) {
+        this.name = name;
+        this.employeeId = employeeId;
+        this.department = department;
+        this.payeRate = payeRate;
+        this.hoursWorked = hoursWorked;
         this.totalPay = totalPay;
+        this.regularHours = regularHours;
+        this.overTimeHours = overTimeHours;
+        this.punchOut = punchOut;
+        this.punchIn = punchIn;
     }
 
-    //===================================
-    // Getter Methods
-    //===================================
 
+    ///===================================
+            /// Getter Methods
+    ///===================================
 
-    // Get THe Total Payment
+    /// Get The Employee Name
+    public String getName() {
+        return name;
+    }
+
+    /// Set The Employee Name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /// Get The Employee id
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    /// Set THe Employee id
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    /// Get The Department
+    public String getDepartment() {
+        return department;
+    }
+
+    /// Set The Department
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    /// Get The Pay Rate
+    public float getPayeRate() {
+        return payeRate;
+    }
+
+    /// Get THe Number Of Hours Worked
+    public float getHoursWorked() {
+        return hoursWorked;
+    }
+
+    /// Set The Total Hours Worked
+    public void setHoursWorked(float hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    /// Get THe Total Payment
     public float getTotalPay() {
         return totalPay;
     }
 
 
-    // Get The Regular Number Of Hours
+    /// Get The Regular Number Of Hours
     public float getRegularHours() {
         return regularHours;
     }
 
-    // Get Over Time Hours
+    /// Get Over Time Hours
     public float getOverTimeHours() {
         return overTimeHours;
+    }
+
+    ///  Check if the employee is punch out or not
+    public double isPunchOut() {
+        return punchOut;
+    }
+
+    ///  set the punch out employee
+    public void setPunchOut(double punchOut) {
+        this.punchOut = punchOut;
+    }
+
+    ///  check if the employee is punched in or not
+    public double isPunchIn() {
+        return punchIn;
+    }
+
+    /// set the employee to punch in
+    public void setPunchIn(double punchIn) {
+        this.punchIn = punchIn;
     }
 }
--- HotelOperations/src/main/java/com/plurasight/Room.java
+++ HotelOperations/src/main/java/com/plurasight/Room.java
@@ -1,37 +1,43 @@
 package com.plurasight;
-// This is The Room Class
-// It describes How Each Room Should be Shapped And Looked Like
+//// This is The Room Class
+//// It describes How Each Room Should be Shapped And Looked Like
 public class Room {
 
-    // Room Main Variables
+    //// Room Main Variables
     private int numberOfBeds;
     private float price;
     private boolean isOccupied;
     private boolean isAvailable;
+    private boolean isDirty;
     private int roomId;
+    private boolean checkIn;
+    private boolean checkOut;
 
-    // Room Constructor To Re-assign The Values To The Room Variables
-    public Room(int numberOfBeds, boolean isAvailable, boolean isOccupied, float price, int roomId) {
+    //// Room Constructor To Re-assign The Values To The Room Variables
+    public Room(int numberOfBeds, boolean isAvailable, boolean isOccupied, float price, int roomId, boolean isDirty, boolean checkIn, boolean checkOut) {
         this.numberOfBeds = numberOfBeds;
         this.isAvailable = isAvailable;
         this.isOccupied = isOccupied;
         this.price = price;
         this.roomId = roomId;
+        this.isDirty = isDirty;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
     }
 
-    // Getter Methods For The Variables
+    //// Getter Methods For The Variables
 
-    // Get Number Of Bes In Each Room
+    //// Get Number Of Bes In Each Room
     public int getNumberOfBeds() {
         return numberOfBeds;
     }
 
-    // To Check If THe Room Is Available Or Not
+    //// To Check If THe Room Is Available Or Not
     public boolean isAvailable() {
         return isAvailable;
     }
 
-    // To Get The Room Price
+    //// To Get The Room Price
     public float getPrice() {
         return price;
     }
@@ -41,8 +47,38 @@ public class Room {
         return isOccupied;
     }
 
-    // Get Room id
+    //// Get Room id
     public int roomId(int roomId){
         return roomId;
+    }
+
+    //// Get If THe Room Is Dirty Or Not
+    public boolean isDirty() {
+        return isDirty;
+    }
+
+    //// Set The Room Is Dirty Or Not
+    public void setDirty(boolean dirty) {
+        isDirty = dirty;
+    }
+
+    //// Check If THe Room Is Checked Out Or Not
+    public boolean isCheckOut() {
+        return checkOut;
+    }
+
+    //// Set The Check In Room
+    public void setCheckOut(boolean checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    //// Check If THe Room Is Checked In Or Not
+    public boolean isCheckIn() {
+        return checkIn;
+    }
+
+    //// Set The Check In Room
+    public void setCheckIn(boolean checkIn) {
+        this.checkIn = checkIn;
     }
 }
